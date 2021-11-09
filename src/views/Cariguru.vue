c544713b24a14bdf502d9a37e71c40c648c4ca00
Haris Putratama <lastblezebub@gmail.com>
2021-11-09 13:15:14 +0700

    added vue router

---
+++ src/views/Cariguru.vue
@@ -0,0 +1,463 @@
+<template>
+  <v-app id="app">
+    <v-app-bar app light class="pl-7" height="80" id="navbar" flat>
+      <a class="navbar-brand" href="#">
+        <img src="../assets/Dreamguru.png" width="136" height="28" alt="">
+      </a>
+      <v-spacer></v-spacer>
+      <v-container class="">
+        <v-row no-gutters >
+          <v-col cols="12" sm="10">
+          </v-col>
+          <v-col cols="12" sm="1" no-gutters class="text-end">
+            <v-avatar>
+                <img
+                src="../assets/avatar.png"
+                alt="John"
+                size="46"
+                >
+            </v-avatar>
+          </v-col>
+          <v-col cols="12" sm="1" no-gutters class="text-start">
+            <p class="mt-1 mb-0 ml-2"><b>Robert P.</b></p>
+            <p class="xsmall mb-0 ml-2">Murid</p>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-btn
+              class="mr-4"
+              fab
+              light
+              style="height: 48px; width:48px; box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.06);"
+              color="white"
+            >
+              <v-icon light>
+                mdi-bell-outline
+              </v-icon>
+            </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <h1 class="text-start hero-1">Cari <span style="color:#FFC555; font-weight:700;">Guru </span> Yang Pas Untukmu !</h1>
+          <v-col cols="12">
+            <v-text-field
+            rounded
+            hint="Coba, Cari guru matematika!"
+            label="Cari Guru"
+            outlined
+            clearable
+          ></v-text-field>
+          </v-col>
+          <v-row>
+            <v-col cols="12" sm="1" class="text-start">
+              <h5 class="mt-4"><b>Filter By : </b></h5>
+            </v-col>
+            <v-col cols="12" sm="2" class="text-start">
+              <v-select
+              class="filter"
+              rounded
+              :items="BidangStudi"
+              label="Bidang Studi"
+              solo
+              ></v-select>
+            </v-col>
+            <v-col cols="12" sm="2" class="text-start">
+              <v-select
+              class="filter"
+              rounded
+              :items="harga"
+              label="Harga"
+              solo
+              ></v-select>
+            </v-col>
+            <v-col cols="12" sm="2" class="text-start">
+              <v-select
+              class="filter"
+              rounded
+              :items="rating"
+              label="Rating"
+              solo
+              ></v-select>
+            </v-col>
+            <v-col cols="12" sm="2" class="text-start">
+              <v-select
+              class="filter"
+              rounded
+              :items="availability"
+              label="Availability"
+              solo
+              ></v-select>
+            </v-col>
+          </v-row>
+        <h1 class="text-start hero-2">Guru Terkait </h1>
+        <v-row no-gutters> 
+          <v-col cols="12" sm="2" class="text-start">
+        <v-dialog max-width="428">
+          <template v-slot:activator="{ on, attrs }">
+            <v-card max-width="170" class="cards" v-bind="attrs" v-on="on">
+              <v-avatar color="#E6696A" class="ma-3 cards-avatar">
+              <img src="../assets/budi.png" alt="Budi">
+              </v-avatar>
+            <p class="ml-3 mb-0"><b>Budi Santoso</b></p>
+            <p class="small ml-3 mb-0">Matematika</p>
+            <v-rating class="ml-3"
+              background-color="grey lighten-1"
+              color="warning"
+              hover
+              length="5"
+              readonly
+              dense
+              half-increments
+              size="18"
+              value= "5"
+            ></v-rating>
+            <p class="ml-3 pb-5"><b>Rp. 350.000 / session</b></p>
+            </v-card>
+              </template>
+              <template v-slot:default="dialog">
+                  <v-card class="bg"> 
+                    <v-card-text>
+                  <v-container> 
+                    <v-row>
+                      <v-col cols="12" sm="6" class="text-end" no-gutters style="padding-right:0px;">
+                        <v-avatar color="#E6696A" class="ma-3 cards-avatar">
+                        <img src="../assets/budi.png" alt="Budi">
+                        </v-avatar>
+                      </v-col>
+                      <v-col cols="12" sm="6" class="text-start" style="padding-left:0px;" no-gutters>
+                        <p class="mt-3 mb-0"><b>Budi Santoso</b></p>
+                        <p class="small mb-0">Matematika</p>
+                        <v-rating class=""
+                          background-color="grey lighten-1"
+                          color="warning"
+                          hover
+                          length="5"
+                          readonly
+                          dense
+                          half-increments
+                          size="18"
+                          value= "5"
+                        ></v-rating>
+                        <p class="mb-0"><b>Rp. 350.000 / session</b></p>
+                        <img class="ml-0" src="../assets/prudent.png"> <img class="ml-1" src="../assets/humble.png"> <v-spacer></v-spacer> 
+                        <img class="" src="../assets/focused.png">
+                      </v-col>
+                    </v-row>
+                    <v-row> 
+                      <v-col cols="12" sm="6" class="text-end" no-gutters style="padding: 6px;">
+                        <img class="" src="../assets/graph.png">
+                      </v-col>
+                      <v-col cols="12" sm="6" class="text-start" style="padding-left:0px; padding-right:0px;" no-gutters>
+                        <h6><b>Terbuka & Ramah </b></h6>
+                        <p class="xsmall">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Est nibh in pulvinar sit ornare et in eget. Consequat mi vitae amet aliquam mauris eu. </p>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions class="px-8">
+                  <v-btn
+                    color="#FDC356" depressed rounded dark block
+                    @click="dialog.value = false"
+                    href="payment.html"
+                  >
+                    Request
+                  </v-btn>
+                </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+          </v-col>
+          <v-col cols="12" sm="2" class="text-start">
+            <v-card max-width="170" class="cards">
+              <v-avatar color="#FFC555" class="ma-3 cards-avatar">
+              <img src="../assets/andi.png" alt="Andi">
+              </v-avatar>
+            <p class="ml-3 mb-0"><b>Andi Wijaya</b></p>
+            <p class="small ml-3 mb-0">Matematika</p>
+            <v-rating class="ml-3"
+              background-color="grey lighten-1"
+              color="warning"
+              hover
+              length="5"
+              readonly
+              dense
+              half-increments
+              size="18"
+              value= "5"
+            ></v-rating>
+            <p class="ml-3 pb-5"><b>Rp. 350.000 / session</b></p>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="2" class="text-start">
+            <v-card max-width="170" class="cards">
+              <v-avatar color="#38BEA5" class="ma-3 cards-avatar">
+              <img src="../assets/susi.png" alt="Susi">
+              </v-avatar>
+            <p class="ml-3 mb-0"><b>Susi Susanti</b></p>
+            <p class="small ml-3 mb-0">Matematika</p>
+            <v-rating class="ml-3"
+              background-color="grey lighten-1"
+              color="warning"
+              hover
+              length="5"
+              readonly
+              dense
+              half-increments
+              size="18"
+              value= "5"
+            ></v-rating>
+            <p class="ml-3 pb-5"><b>Rp. 350.000 / session</b></p>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="2" class="text-start">
+            <v-card max-width="170" class="cards">
+              <v-avatar color="#E6696A" class="ma-3 cards-avatar">
+              <img src="../assets/budi.png" alt="Budi">
+              </v-avatar>
+            <p class="ml-3 mb-0"><b>Budi Santoso</b></p>
+            <p class="small ml-3 mb-0">Matematika</p>
+            <v-rating class="ml-3"
+              background-color="grey lighten-1"
+              color="warning"
+              hover
+              length="5"
+              readonly
+              dense
+              half-increments
+              size="18"
+              value= "5"
+            ></v-rating>
+            <p class="ml-3 pb-5"><b>Rp. 350.000 / session</b></p>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="2" class="text-start">
+            <v-card max-width="170" class="cards">
+              <v-avatar color="#FFC555" class="ma-3 cards-avatar">
+              <img src="../assets/andi.png" alt="Andi">
+              </v-avatar>
+            <p class="ml-3 mb-0"><b>Andi Wijaya</b></p>
+            <p class="small ml-3 mb-0">Matematika</p>
+            <v-rating class="ml-3"
+              background-color="grey lighten-1"
+              color="warning"
+              hover
+              length="5"
+              readonly
+              dense
+              half-increments
+              size="18"
+              value= "5"
+            ></v-rating>
+            <p class="ml-3 pb-5"><b>Rp. 350.000 / session</b></p>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="2" class="text-start">
+            <v-card max-width="170" class="cards">
+              <v-avatar color="#38BEA5" class="ma-3 cards-avatar">
+              <img src="../assets/susi.png" alt="Susi">
+              </v-avatar>
+            <p class="ml-3 mb-0"><b>Susi Susanti</b></p>
+            <p class="small ml-3 mb-0">Matematika</p>
+            <v-rating class="ml-3"
+              background-color="grey lighten-1"
+              color="warning"
+              hover
+              length="5"
+              readonly
+              dense
+              half-increments
+              size="18"
+              value= "5"
+            ></v-rating>
+            <p class="ml-3 pb-5"><b>Rp. 350.000 / session</b></p>
+            </v-card>
+          </v-col>
+        </v-row>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+  
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+        BidangStudi: ['Matematika', 'Biologi', 'Kimia', 'Fisika'],
+        harga:['dibawah 50.000','50.000 - 100.000','100.000 - 300.000','diatas 300.000'],
+        rating:['1', '2', '3', '4', '5'],
+        availability:['Setiap Hari','2 Hari Sekali','1 Minggu Sekali'],
+
+        show1: false,
+        show2: true,
+        show3: false,
+        show4: false,
+        password: 'Password',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+        components: {
+            
+        }
+      }),
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+#app {
+  font-family: "Montserrat";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-image: url("../assets/bg.png");
+  background-repeat: no-repeat;
+  background-position: 95% 40%;
+  background-color: #F9FAFB;
+}
+p.xsmall{
+  font-size: 10px !important;
+}
+p.small{
+  font-size: 12px !important;
+}
+p{
+  font-size: 14px !important;
+}
+h1{
+  font-size: 48px !important;
+}
+h2{
+  font-size: 36px !important;
+}
+h3{
+  font-size: 28px !important;
+}
+h4{
+  font-size: 22px !important;
+}
+h5{
+  font-size: 18px !important;
+}
+h6{
+  font-size: 16px !important;
+}
+.cards{
+  border-radius: 20px !important;
+  margin-right: 0px !important;
+}
+.cards-avatar{
+  width: 144px !important;
+  height: 139px !important;
+  border-radius: 15px !important;
+}
+.form-title{
+  padding: 40px;
+  padding-bottom: 20px;
+}
+.hero-1-content{
+  text-align: start;
+  padding: 30px !important;
+  padding-top: 100px !important;
+  margin-left: 100px;
+}
+.hero-1{
+  margin-top: 128px !important;
+  margin-bottom: 44px !important;
+}
+.hero-2{
+  margin-top: 50px !important;
+  margin-bottom: 30px !important;
+}
+.bg{
+  background: #F9FAFB !important;
+}
+.btn-google{
+
+box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.084), 0px 1px 1px rgba(0, 0, 0, 0.168)!important;
+border-radius: 2px!important;
+}
+.container-fluid.hero-2{
+  margin-top: 50px;
+}
+.btn.btn-outline-primary{
+  border-radius: 18px;
+  border: 2px solid #FFC555;
+  color:#FFC555;
+  padding-left:30px;
+  padding-right:30px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.btn.btn-outline-primary:hover{
+  background:#FFC555;
+  border: 2px solid #FFC555;
+}
+.btn.btn-primary:hover{
+  background:#E3A733;
+  border: 2px solid #E3A733;
+}
+.btn.btn-primary{
+  border-radius: 18px;
+  border: 2px solid #FFC555;
+  background:#FFC555;
+  padding-left:30px;
+  padding-right:30px;
+  margin-left: 10px;
+  margin-right: 30px;
+}
+.container-hero-2{
+  margin : 15px;
+  margin-top: 50px !important;
+  margin-bottom: 50px !important;
+}
+.container-pengajar{
+  height: 146px;
+
+  background: #FFFFFF;
+  box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.06);
+  border-radius: 20px;
+}
+.container-photo-1{
+  height: 146px;
+  background: #E6696A;
+  border-radius: 20px 0px 0px 20px;
+  object-fit: cover;
+  overflow:hidden;
+  padding:0px !important;
+}
+.container-photo-2{
+  height: 146px;
+  background: #38BEA5;
+  border-radius: 20px 0px 0px 20px;
+  object-fit: cover;
+  overflow:hidden;
+  padding:0px !important;
+}
+.container-photo-3{
+  height: 146px;
+  background: #FFC555;
+  border-radius: 20px 0px 0px 20px;
+  object-fit: cover;
+  overflow:hidden;
+  padding:0px !important;
+}
+.container-text-pengajar{
+  padding: 15px !important;
+}
+
+#navbar{
+  background-color: #F9FAFB;
+  filter: drop-shadow(0px 4px 40px rgba(0, 0, 0, 0.06));
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,376 @@
+<template>
+  <v-app id="app">
+    <v-app-bar app light class="pl-7" height="80" id="navbar" flat>
+      <a class="navbar-brand" href="#">
+        <img src="../assets/Dreamguru.png" width="136" height="28" alt="">
+      </a>
+      <v-spacer></v-spacer>
+      <div class="hidden-sm-and-down">
+        <v-btn class="mr-5 px-7" color="#FDC356" rounded light outlined>Untuk Guru</v-btn>
+        <v-btn class="mr-5 px-7" color="#FDC356" rounded light outlined>Daftar</v-btn>
+
+        <!-- login button -->
+        <v-dialog persistent max-width="600">
+
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            class="px-7" color="#FDC356" depressed rounded dark
+            v-bind="attrs"
+            v-on="on"
+          >Masuk</v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card class="bg">
+            <div class="form-title">
+              <img src="../assets/Dreamguru.png" width="184" height="38" alt="">
+              <br><br><br>
+              <h3 style="font-weight: 600;">Masuk</h3>
+            </div>
+            
+            <v-card-text>
+          <v-container class="px-10">
+            <v-row no-gutters>
+              <v-col cols="12">
+                <p class="text-left font-weight-bold">Email atau Username</p>
+                <v-text-field
+                  solo
+                  clearable
+                  label="john.doe@gmail.com"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <p class="text-left font-weight-bold">Password</p>
+                <v-text-field
+                  v-model="password"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.required, rules.min]"
+                  :type="show1 ? 'text' : 'password'"
+                  name="input-10-1"
+                  label="Masukkan Kata Sandi"
+                  hint="At least 8 characters"
+                  counter
+                  @click:append="show1 = !show1"
+                  solo
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions class="px-16">
+          <v-btn x-large
+            color="#FDC356" depressed rounded dark block
+            @click="dialog.value = false"
+            href="#/cariguru"
+          >
+            Masuk
+          </v-btn>
+        </v-card-actions>
+        <v-card-actions class="px-16 pb-6">
+          <v-btn x-large class="btn-google"
+            color="#FFFFFF" block depressed
+            @click="dialog.value = false"
+            href="#/cariguru"
+          >
+          <div class="left">
+                <img width="20px" style="margin-right:20px" alt="Google sign-in" 
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+            </div>
+            Masuk dengan Google
+          </v-btn>
+          
+        </v-card-actions>
+        <p class="pb-16 mb-0">Belum punya akun? 
+          <v-btn class="pa-0" color="#FDC356" plain text>Daftar Sekarang</v-btn> </p>
+          </v-card>
+        </template>
+      </v-dialog>
+      </div>
+      <!-- Hamburger Menu -->
+      <div class="hidden-md-and-up">
+      <v-menu bottom left>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                light
+                icon
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <div id="app">
+      <main>
+
+        <div class="container-fluid hero-1">
+          <div class="row">
+            <div class ="col-md-4 hero-1-content">
+              <h1>Belajar online jadi lebih asik, <b>Kalo gurunya klop!</b></h1>
+              <br>
+              <h4>Bersama DreamGuru, kamu bisa nemuin guru yang satu pemikiran dan ga ngebosenin lho, 
+                Kok bisa? Makanya yuk <b>cobain daftar sekarang juga!</b> </h4>
+              <v-btn class="my-5 px-7" color="#FDC356" depressed rounded dark>Masuk</v-btn>
+            </div>
+          </div>
+        </div>
+
+        <div class="container-fluid hero-2">
+          <div class="row">
+            <div class ="col hero-2-content">
+              <h1><b>Pengajar Terbaik Kami</b></h1>
+              <br>
+            </div>
+          </div>
+          <div class="flex-row d-flex justify-content-center">
+            <div class ="col-md-3 container-hero-2">
+              <div class="row container-pengajar">
+                <div class="col-sm-4 container-photo-1">
+                    <div id="avatar1"></div>
+                </div>
+                <div class="col-sm-8 text-start container-text-pengajar">
+                  <p><b>Budi Santoso</b></p>
+                  <p class="xsmall">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet malesuada sagittis nam feugiat. Ut diam elit et volutpat adipiscing mi egestas. Cras volutpat.</p>
+                </div>
+              </div>
+            </div>
+            <div class ="col-md-3 container-hero-2">
+              <div class="row container-pengajar">
+                <div class="col-sm-4 container-photo-2">
+                    <div id="avatar2"></div>
+                </div>
+                <div class="col-sm-8 text-start container-text-pengajar">
+                  <p><b>Susi Susanti</b></p>
+                  <p class="xsmall">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet malesuada sagittis nam feugiat. Ut diam elit et volutpat adipiscing mi egestas. Cras volutpat.</p>
+                </div>
+              </div>
+            </div>
+            <div class ="col-md-3 container-hero-2">
+              <div class="row container-pengajar">
+                <div class="col-sm-4 container-photo-3">
+                    <div id="avatar3"></div>
+                </div>
+                <div class="col-sm-8 text-start container-text-pengajar">
+                  <p><b>Andi Wijaya</b></p>
+                  <p class="xsmall">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet malesuada sagittis nam feugiat. Ut diam elit et volutpat adipiscing mi egestas. Cras volutpat.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+
+    </v-main>
+  </v-app>
+  
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  components: {
+  },
+
+  data: () => ({
+        items: [
+          { title: 'Masuk' },
+          { title: 'Daftar' },
+          { title: 'Untuk Guru' },
+        ],
+        show1: false,
+        show2: true,
+        show3: false,
+        show4: false,
+        password: 'Password',
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+          emailMatch: () => (`The email and password you entered don't match`),
+        },
+        components: {
+            
+        }
+      }),
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+#app {
+  font-family: "Montserrat";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #F9FAFB;
+}
+p.xsmall{
+  font-size: 10px !important;
+}
+p{
+  font-size: 14px !important;
+}
+h1{
+  font-size: 48px !important;
+}
+h2{
+  font-size: 36px !important;
+}
+h3{
+  font-size: 28px !important;
+}
+h4{
+  font-size: 22px !important;
+}
+h5{
+  font-size: 18px !important;
+}
+h6{
+  font-size: 16px !important;
+}
+.form-title{
+  padding: 40px;
+  padding-bottom: 20px;
+}
+.hero-1-content{
+  text-align: start;
+  padding: 30px !important;
+  padding-top: 100px !important;
+  margin-left: 100px;
+}
+.btn-hero-1{
+  margin-left: 0px !important;
+  margin-top: 20px;
+}
+.bg{
+  background: #F9FAFB !important;
+}
+.btn-google{
+
+box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.084), 0px 1px 1px rgba(0, 0, 0, 0.168)!important;
+border-radius: 2px!important;
+}
+.container-fluid.hero-1{
+  background-image: url("../assets/Frame 1.png");
+  background-repeat: no-repeat;
+  background-size: initial;
+  background-position: right bottom;
+  overflow: hidden;
+}
+.container-fluid.hero-2{
+  margin-top: 50px;
+}
+.btn.btn-outline-primary{
+  border-radius: 18px;
+  border: 2px solid #FFC555;
+  color:#FFC555;
+  padding-left:30px;
+  padding-right:30px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+.btn.btn-outline-primary:hover{
+  background:#FFC555;
+  border: 2px solid #FFC555;
+}
+.btn.btn-primary:hover{
+  background:#E3A733;
+  border: 2px solid #E3A733;
+}
+.btn.btn-primary{
+  border-radius: 18px;
+  border: 2px solid #FFC555;
+  background:#FFC555;
+  padding-left:30px;
+  padding-right:30px;
+  margin-left: 10px;
+  margin-right: 30px;
+}
+.container-hero-2{
+  margin : 15px;
+  margin-top: 50px !important;
+  margin-bottom: 50px !important;
+}
+.container-pengajar{
+  height: 146px;
+
+  background: #FFFFFF;
+  box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.06);
+  border-radius: 20px;
+}
+.container-photo-1{
+  height: 146px;
+  background: #E6696A;
+  border-radius: 20px 0px 0px 20px;
+  object-fit: cover;
+  overflow:hidden;
+  padding:0px !important;
+}
+.container-photo-2{
+  height: 146px;
+  background: #38BEA5;
+  border-radius: 20px 0px 0px 20px;
+  object-fit: cover;
+  overflow:hidden;
+  padding:0px !important;
+}
+.container-photo-3{
+  height: 146px;
+  background: #FFC555;
+  border-radius: 20px 0px 0px 20px;
+  object-fit: cover;
+  overflow:hidden;
+  padding:0px !important;
+}
+.container-text-pengajar{
+  padding: 15px !important;
+}
+#avatar1 {
+    background-image: url('../assets/budi.png');
+    width: 100%;
+    height: 100%;
+    border-radius: 20px 0px 0px 20px;
+    background-size: auto;
+    background-repeat: no-repeat;
+    background-position: bottom center;
+}
+#avatar2 {
+    background-image: url('../assets/susi.png');
+    width: 100%;
+    height: 100%;
+    border-radius: 20px 0px 0px 20px;
+    background-size: auto;
+    background-repeat: no-repeat;
+    background-position: bottom center;
+}
+#avatar3 {
+    background-image: url('../assets/andi.png');
+    width: 100%;
+    height: 100%;
+    border-radius: 20px 0px 0px 20px;
+    background-size: auto;
+    background-repeat: no-repeat;
+    background-position: bottom center;
+}
+
+#navbar{
+  background-color: #F9FAFB;
+  filter: drop-shadow(0px 4px 40px rgba(0, 0, 0, 0.06));
+}
+</style>
